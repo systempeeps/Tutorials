db14286a0eb765754f810cdf33d0db2b759c45dd
Kris Peterson <systempeeps@gmail.com>
2016-01-20 22:24:32 -0600

    [added]Added variables example for tutorial

---
+++ C/basics/variables01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 1;
+    int b = 2, c = 3, d = 4;
+
+
+    char charvar = 'c';
+
+    printf("Inter value is %d %d %d %d\n", a, b, c, d);
+
+    printf("Character value %c\n", charvar);
+
+    return 0;
+}
