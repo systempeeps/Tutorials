d65e7ae2efa744095d8f8306047d9430f776d841
Kris Peterson <systempeeps@gmail.com>
2016-01-21 22:11:15 -0600

    [added]Added expression example

---
+++ C/basics/expression.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+
+	int num1 = 10, num2 = 3;
+
+	int sum = num1 + num2;
+	int difference = num1 - num2;
+	int product = num1 * num2;
+	int quotient = num1 / num2;
+
+
+	printf("Sum = %d\n", sum);
+	printf("Difference = %d\n", difference);
+	printf("Product = %d\n", product);
+	printf("Quotient = %d\n", quotient);
+
+	return 0;
+
+}
