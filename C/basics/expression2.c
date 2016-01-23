d305e09c0a298cddbf62026b9dc8e48ec7fac6a2
Kris Peterson <systempeeps@gmail.com>
2016-01-23 10:44:57 -0600

    [added]Added float point expressions example in C

---
+++ C/basics/expression2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+
+	float num1 = 10, num2 = 3.1;
+
+	int sum = num1 + num2;
+	int difference = num1 - num2;
+	int product = num1 * num2;
+	float quotient = num1 / num2;
+
+
+	printf("Sum = %d\n", sum);
+	printf("Difference = %d\n", difference);
+	printf("Product = %d\n", product);
+	printf("Quotient = %f\n", quotient);
+
+	return 0;
+
+}
