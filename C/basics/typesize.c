f88b9faf6c592055b4950688c289edcd4ec5966e
Kris Peterson <systempeeps@gmail.com>
2016-01-23 13:54:50 -0600

    [added]Added data type size example in C

---
+++ C/basics/typesize.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("Size of int is %zu\n", sizeof(int));
+	printf("Size of long is %zu\n", sizeof(long));
+	printf("Size of float is %zu\n", sizeof(float));
+	printf("Size of char is %zu\n", sizeof(char));
+
+	return 0;
+}
