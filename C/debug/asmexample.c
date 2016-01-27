0335bc897490f408dc88db64dc033974232a2c01
Kris Peterson <systempeeps@gmail.com>
2016-01-26 20:47:37 -0600

    [added]Add C gdb asm example

---
+++ C/debug/asmexample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+
+	int a = 0;
+	int b = 1;
+	int c = 2;
+	int d = 3;
+	a += 1;
+	a += b;
+	a = a + c;
+	
+	printf("The value of a is %d\n", a);
+
+	return 0;
+
+}
